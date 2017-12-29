5a1a7f87ddfd0d4555e369f097ac9d38762f58ba
yukixoma <yukixoma08@gmail.com>
2017-12-29 15:25:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,68 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Color from './components/color';
-import Size from './components/size';
-import Display from './components/display';
-import Reset from './components/reset';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: "#000000",
-      size: 50,
-    }
-  }
-
-  onSetColor = (color) => {
-    this.setState({ color: color});
-  }
-
-  onSetSize = (size) => {
-    this.setState({size: size});
-  }
-
-  onReset = () => {
-    this.setState({
-      color: "#000000",
-      size: 50,
-    })
-  }
   render() {
     return (
       <div>
         <header className="App-header text-center">
           <img src={logo} className="App-logo" alt="logo" /> 
-          <h1 className="App-title ">Welcome to React</h1> 
+          <h1 className="App-title "> Simple Manager App </h1> 
         </header>
-        <br/>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6">
-              <Color
-                color           = { this.state.color } 
-                onReceiveColor  = { this.onSetColor }
-              />
-            </div>
-            <div className="col-lg-6">
-              <Size 
-                size          = { this.state.size }
-                onReceiveSize = { this.onSetSize }
-              />
-            </div>
-          </div>
-          <br/>
-          <div>
-            <Display 
-              color = { this.state.color }
-              size  = { this.state.size } 
-            />
-          </div>
-          <div className="text-center">
-            <Reset onReceiveReset = { this.onReset }/>
-          </div>
-        </div>
       </div>
     );
   }
